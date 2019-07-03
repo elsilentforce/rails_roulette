afee23e2335b3099f6d7383a349a9a9df53b599c
Patricio Tapia <ptapiaugalde@gmail.com>
2019-07-03 12:34:50 -0400

    ADD: Models and database logic

---
+++ db/migrate/20190703161618_create_matches.rb
@@ -0,0 +1,9 @@
+class CreateMatches < ActiveRecord::Migration
+  def change
+    create_table :matches do |t|
+      t.references :user, index: true, foreign_key: true
+      t.references :game, index: true, foreign_key: true
+      t.timestamps null: false
+    end
+  end
+end
