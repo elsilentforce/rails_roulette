d2d1d4d7624504111cf9d9e6ab2605f36d7b7ce4
Patricio Tapia <ptapiaugalde@gmail.com>
2019-07-04 10:00:58 -0400

    ADD: RSpec & Capybara

--- db/schema.rb
+++ db/schema.rb
@@ -32,10 +32,10 @@ ActiveRecord::Schema.define(version: 20190703161618) do
   add_index "matches", ["user_id"], name: "index_matches_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
-    t.string   "username",                      null: false
-    t.string   "current_credits", default: "0"
-    t.datetime "created_at",                    null: false
-    t.datetime "updated_at",                    null: false
+    t.string   "username",                          null: false
+    t.string   "current_credits", default: "10000"
+    t.datetime "created_at",                        null: false
+    t.datetime "updated_at",                        null: false
   end
 
   add_index "users", ["username"], name: "index_users_on_username", unique: true, using: :btree
