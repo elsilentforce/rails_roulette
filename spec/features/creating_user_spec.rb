5f2d590312043d3833f56b267a710a778c543cff
Patricio Tapia <ptapiaugalde@gmail.com>
2019-07-04 22:12:06 -0400

    REFACT: Guardfile

--- spec/features/creating_user_spec.rb
+++ spec/features/creating_user_spec.rb
@@ -15,15 +15,4 @@ RSpec.feature "Creating User:" do
     expect(page).to have_current_path( user_path(User.last) )
   end
 
-  scenario "Someone tries to create a user with existing username" do
-    visit "/"
-    click_link "New User"
-
-    fill_in "Username", with: test_user.username
-
-    click_button "Create User"
-
-    expect(page).to have_content("User was successfully created.")
-    expect(page).to have_current_path( user_path(User.last) )
-  end
 end
