1f6c5a7f1672a72e9a19f63e2c16ad7f446f806e
Patricio Tapia <ptapiaugalde@gmail.com>
2019-07-11 00:27:18 -0400

    REFACT: Unnecesary code

--- app/controllers/rounds_controller.rb
+++ app/controllers/rounds_controller.rb
@@ -1,20 +1,2 @@
 class RoundsController < ApplicationController
-
-  def new
-
-  end
-
-  def create
-    User.each do |user|
-      current_bet = set_current_bet(user)
-      Round.create(user_id: user.id, game_id: game.id, bet: current_bet)
-      user.update(credits: (user.credits - current_bet) )
-    end
-  end
-
-  private
-  def set_game
-    @game = Game.find(params[:game_id])
-  end
-
 end
